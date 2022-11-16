7e042dfdd5315f6178b7f5357f573b5a18fa3d22
mogeritz <moge.world.kanal@gmail.com>
2022-11-15 18:07:17 -0800

    Added Javascript Backend for delete functionality
    Co-authored by Ziting Xiong

---
+++ scripts/delete.js
@@ -0,0 +1,25 @@
+import { getTasksFromStorage, saveTaskToStorage } from "./add.js";
+
+/**
+ * Reads 'to-do-tasks' from localStorage, removes the task with the given ID
+ * And then rewrites the updated list of Tasks back to local storage.
+ * @param {number} id ID of the task to be removed from local storage
+ */
+export function deleteTaskById(id){
+
+    const taskList = getTasksFromStorage();
+    
+    //const del = document.getElementById('id');
+
+    for(var i = 0; i < taskList.length; i++) {
+        /**if(taskList[i] == del) {
+            taskList.splice(i, 1);
+        }**/
+
+        if(taskList[i].id == id){
+            taskList.splice(i, 1);
+        }
+    }
+
+    saveTaskToStorage(taskList);
+}
