6c46824ce85a9806fed0c60fd37fb65c4bf7baae
Melvyn <42531205+melvyn9@users.noreply.github.com>
2022-11-30 15:30:00 -0800

    Testing CI/CD Pipeline

--- source/__tests__/sum.test.js
+++ source/__tests__/sum.test.js
@@ -3,3 +3,7 @@ const sum = require('../scripts/sum.js')
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3)
 })
+
+test('adds 5 + 6 to equal 3', () => {
+    expect(sum(5, 6)).toBe(11)
+})
