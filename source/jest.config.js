2829e58a9cfab075b7c563abbe00d2c532b6b4c0
mogeritz <mogeritz@users.noreply.github.com>
2022-12-03 00:41:08 +0000

    Enforced Programming Protocols

--- source/jest.config.js
+++ source/jest.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    preset: 'ts-jest',
-    roots: ['<rootDir>/src'],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    transform: {
-        '\\.(ts|tsx)$': 'ts-jest',
-    },
-    setupFiles: ['raf/polyfill'],
-    testRegex: '/__tests__/.*\\.(ts|tsx|js)$',
-    moduleNameMapper: {
-        '^.+\\.(s?css|less|jpg|png|svg)$': 'identity-obj-proxy',
-    },
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-    snapshotSerializers: [],
-}
+	preset: "ts-jest",
+	roots: ["<rootDir>/src"],
+	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+	transform: {
+		"\\.(ts|tsx)$": "ts-jest",
+	},
+	setupFiles: ["raf/polyfill"],
+	testRegex: "/__tests__/.*\\.(ts|tsx|js)$",
+	moduleNameMapper: {
+		"^.+\\.(s?css|less|jpg|png|svg)$": "identity-obj-proxy",
+	},
+	setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+	snapshotSerializers: [],
+};
